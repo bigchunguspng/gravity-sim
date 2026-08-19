63339265220b6d351286e239b61df9c32ef7756b
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2026-08-19 12:25:39 +0300

    raylib

---
+++ GravitySimulator/Game.cs
@@ -0,0 +1,21 @@
+﻿using Raylib_cs;
+
+namespace GravitySimulator;
+
+public class Game
+{
+    public void Run()
+    {
+        Raylib.InitWindow(640, 480, "Title");
+        Raylib.SetTargetFPS(60);
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            {
+                Raylib.ClearBackground(Color.Black);
+            }
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
---
+++ GravitySimulator/Program.cs
@@ -0,0 +1,5 @@
+﻿using GravitySimulator;
+
+var g = new Game();
+
+g.Run();
