65f04c477ea8aa304a135d453b2bff66531f0905
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2026-08-19 14:42:08 +0300

    v1

--- GravitySimulator/Game.cs
+++ GravitySimulator/Game.cs
@@ -4,15 +4,39 @@ namespace GravitySimulator;
 
 public class Game
 {
+    private const int
+        W = 640,
+        H = 480,
+        M = 25;
+
     public void Run()
     {
-        Raylib.InitWindow(640, 480, "Title");
+        Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
+        Raylib.InitWindow(W, H, "Gravity Simulator");
         Raylib.SetTargetFPS(60);
+
+        const int count = 25;
+        var s = new Simulation();
+        s.Init(count, W, H);
+
         while (!Raylib.WindowShouldClose())
         {
+            // LOGIC
+            s.Tick();
+
+            // RENDER
             Raylib.BeginDrawing();
             {
                 Raylib.ClearBackground(Color.Black);
+                for (var i = 0; i < count; i++)
+                {
+                    var x = (int)s.PX[i];
+                    var y = (int)s.PY[i];
+                    var m = s.Mass[i]; // vol sphere = 4/3 pi rrr   r = root3(3 mass / 4 pi)
+                    var r = (float)Math.Pow(0.75 * m / Math.PI, 1 / 3.0);
+                    Raylib.DrawCircle(x, y, r, Color.White);
+                }
+                Raylib.DrawFPS(10, 10);
             }
             Raylib.EndDrawing();
         }
---
+++ GravitySimulator/Simulation.cs
@@ -0,0 +1,69 @@
+﻿namespace GravitySimulator;
+
+public class Simulation
+{
+    public const double G = 1; // 6.6743E-11 too small lol))
+
+    public float[] PX;
+    public float[] PY;
+    public float[] VX;
+    public float[] VY;
+    public float[] AX;
+    public float[] AY;
+    public float[] Mass;
+
+    public void Init(int count, float width, float height)
+    {
+        PX = InitList(count, 0, width);
+        PY = InitList(count, 0, height);
+        VX = InitList(count, 0, 0);
+        VY = InitList(count, 0, 0);
+        AX = new float[count];
+        AY = new float[count];
+        Mass = InitList(count, 1, 10);
+    }
+
+    private static float[] InitList(int count, float min, float max)
+    {
+        var list = new float[count];
+        for (var i = 0; i < count; i++)
+        {
+            list[i] = min + Random.Shared.NextSingle() * (max - min);
+        }
+
+        return list;
+    }
+
+    public void Tick()
+    {
+        var count = Mass.Length;
+        
+        // calculate acceleration
+        for (var i = 0; i < count; i++) // for each particle
+        {
+            AX[i] = AY[i] = 0;
+            for (var j = 0; j < count; j++) // account for gravity to all other particles
+            {
+                if (j == i) continue;
+
+                var dx = PX[i] - PX[j];
+                var dy = PY[i] - PY[j];
+                var d = Math.Sqrt(dx * dx + dy * dy);
+                var ax = G * Mass[j] * dx / (d * d * d);
+                var ay = G * Mass[j] * dy / (d * d * d);
+                AX[i] -= (float)ax;
+                AY[i] -= (float)ay;
+            }
+        }
+
+        for (int i = 0; i < count; i++)
+        {
+            // affect velocity with acceleration
+            VX[i] += AX[i];
+            VY[i] += AY[i];
+            // affect position with velocity
+            PX[i] += VX[i];
+            PY[i] += VY[i];
+        }
+    }
+}
