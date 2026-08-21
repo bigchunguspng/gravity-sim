b487d61d2f2bab25468f73cc32bbb7636ef9cb28
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2026-08-21 16:30:32 +0300

    # { ### }

--- GravitySimulator/Game.cs
+++ GravitySimulator/Game.cs
@@ -20,208 +20,213 @@ public class Game
     private const float
         RADIUS_MULTIPLIER = 1.0F;
 
+    // game state
+    private readonly Simulation s = new();
+    private bool debug = true;
+    private bool pause;
+    private bool step_once;
+    private bool follow_sun = SUN && FOLLOW_SUN_DEFAULT;
+    private Vector2 offset;
+    private readonly Vector2[] positions = new Vector2[COUNT]; // (for convenience)
+
+    // trails
+    private readonly Vector2[] trails    = new Vector2[COUNT * TRAIL_LEN_FRAMES];
+    private readonly int[] trails_lo = new  int[COUNT]; // leftover frames counts for dead particles: 600 -> 0
+    private readonly int[] trails_td = new  int[COUNT]; // tfc value at particle death
+    private int trails_frame_newest; // n
+    private int trails_frame_oldest; // o
+    private int trails_frames_count; // tfc
+
+    private void StartSimulation()
+    {
+        s.Init(COUNT, W, H, SUN);
+    }
+
+    private void ResetTrails()
+    {
+        Array.Clear(trails);
+        Array.Fill(trails_lo, -1);
+        Array.Fill(trails_td, -1);
+        trails_frame_newest = -1;
+        trails_frame_oldest = 0;
+        trails_frames_count = 0;
+    }
+
     public void Run()
     {
         Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
         Raylib.InitWindow(W, H, "Gravity Simulator");
         Raylib.SetTargetFPS(FPS);
 
-        var s = new Simulation();
-        s.Init(COUNT, W, H, SUN);
-
-        var pause = false;
-        var debug = true;
-        var mouse_down_prev_frame = false;
-        var offset = new Vector2();
-        var follow_sun = SUN && FOLLOW_SUN_DEFAULT;
-        var positions = new Vector2[COUNT];
-        var trails    = new Vector2[COUNT * TRAIL_LEN_FRAMES]; // 100p * 10s * 60f/s = 60000 pf * 8B = 480kB
-        var trails_lo = new  int[COUNT]; // leftover frames counts for dead particles: 600 -> 0
-        var trails_td = new  int[COUNT]; // tfc value at particle death
-        int trails_frame_newest; // n
-        int trails_frame_oldest; // o
-        int trails_frames_count; // tfc
-
+        StartSimulation();
         ResetTrails();
-
-        void ResetTrails()
-        {
-            Array.Clear(trails);
-            Array.Fill(trails_lo, -1);
-            Array.Fill(trails_td, -1);
-            trails_frame_newest = -1;
-            trails_frame_oldest = 0;
-            trails_frames_count = 0;
-        }
 
         while (!Raylib.WindowShouldClose())
         {
-            // INPUT
-            var toggle_debug = Raylib.IsKeyPressed(KeyboardKey.F3);
-            if (toggle_debug) debug = !debug;
+            HandleInput();
 
-            var toggle_pause = Raylib.IsKeyPressed(KeyboardKey.Backspace);
-            if (toggle_pause) pause = !pause;
+            if (!pause || step_once) DoLogic();
 
-            var step = pause && Raylib.IsKeyPressed(KeyboardKey.Equal);
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+            DrawTrails();
+            DrawParticles();
+            DrawTextOverlays();
+            Raylib.EndDrawing();
+        }
 
-            var restart = Raylib.IsKeyPressed(KeyboardKey.Enter);
-            if (restart)
-            {
-                s.Init(COUNT, W, H, SUN);
-                offset = new Vector2();
-                ResetTrails();
-            }
+        Raylib.CloseWindow();
+    }
 
-            var toggle_follow_sun = Raylib.IsKeyPressed(KeyboardKey.Backslash);
-            if (toggle_follow_sun)
-            {
-                follow_sun = !follow_sun;
+    private void HandleInput()
+    {
+        var toggle_debug = Raylib.IsKeyPressed(KeyboardKey.F3);
+        if (toggle_debug) debug = !debug;
 
-                if (follow_sun)
-                {
-                    ResetTrails();
-                }
-            }
+        var toggle_pause = Raylib.IsKeyPressed(KeyboardKey.Backspace);
+        if (toggle_pause) pause = !pause;
+
+        step_once = pause && Raylib.IsKeyPressed(KeyboardKey.Equal);
+
+        var restart = Raylib.IsKeyPressed(KeyboardKey.Enter);
+        if (restart)
+        {
+            StartSimulation();
+            ResetTrails();
+            offset = new Vector2();
+        }
+
+        var toggle_follow_sun = Raylib.IsKeyPressed(KeyboardKey.Backslash);
+        if (toggle_follow_sun)
+        {
+            follow_sun = !follow_sun;
 
             if (follow_sun)
             {
-                var c = Raylib.GetScreenCenter();
-                var sun_i = s.SunIndex;
-                offset.X = c.X - s.PX[sun_i];
-                offset.Y = c.Y - s.PY[sun_i];
-
-                mouse_down_prev_frame = false;
-            }
-            else
-            {
-                var mouse_down = Raylib.IsMouseButtonDown(MouseButton.Left);
-                if (mouse_down && !mouse_down_prev_frame)
-                {
-                    mouse_down_prev_frame = true;
-                }
-                else if (mouse_down && mouse_down_prev_frame)
-                {
-                    offset += Raylib.GetMouseDelta();
-                }
-                else
-                {
-                    mouse_down_prev_frame = false;
-                }
-            }
-
-            // LOGIC
-            if (!pause || step)
-            {
-                trails_frame_newest = (trails_frame_newest + 1) % TRAIL_LEN_FRAMES;
-
-                if (trails_frames_count == TRAIL_LEN_FRAMES)
-                    trails_frame_oldest = (trails_frame_oldest + 1) % TRAIL_LEN_FRAMES;
-                else
-                    trails_frames_count++;
-
-                s.Tick();
-
-                for (var i = 0; i < COUNT; i++)
-                {
-                    if (!s.ON[i])
-                    {
-                        ref var
-                            leftover_frames = ref trails_lo[i];
-                        if (leftover_frames == -1)
-                            leftover_frames = trails_td[i] = trails_frames_count;
-                        if (leftover_frames > 0 && trails_frames_count == TRAIL_LEN_FRAMES)
-                            leftover_frames--;
-
-                        continue;
-                    }
-
-                    var x = offset.X + s.PX[i];
-                    var y = offset.Y + s.PY[i];
-                    positions[i] = new Vector2(x, y);
-                    trails[COUNT * trails_frame_newest + i] = new Vector2(x, y);
-                }
-            }
-
-            // RENDER
-            {
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.Black);
-
-                // draw trails
-                for (var i = 0; i < trails_frames_count - 1; i++)
-                {
-                    var f1 = (trails_frame_oldest + i) % TRAIL_LEN_FRAMES;
-                    var f2 =                  (f1 + 1) % TRAIL_LEN_FRAMES;
-
-                    // var fi = i + TRAIL_LEN_FRAMES - trails_frames_count; // used for trail brightness
-                    var rb = (double)i / trails_frames_count; // relative brightness
-                    var v = (byte)(128 * rb);
-                    var color = new Color(v, v, v);
-
-                    for (var j = 0; j < COUNT; j++) // for each particle
-                    {
-                        if (!s.ON[j])
-                        {
-                            var lo = trails_lo[j]; // number of leftover frames
-                            if (lo == 0) continue;
-
-                            var tfc_at_death = trails_td[j];
-                            var skip_particle = trails_frames_count < TRAIL_LEN_FRAMES || tfc_at_death < trails_frames_count
-                                //              ^ buffer not full                      OR particle died when buffer wasn't full
-                                ? i >= tfc_at_death - 2 - trails_frame_oldest
-                                : i >= lo - 1;
-                            if (skip_particle) continue;
-                        }
-
-                        var v1 = trails[COUNT * f1 + j]; // [f0 coords] [f1 coords] [...] [fN coords]
-                        var v2 = trails[COUNT * f2 + j]; //             \---------\ L = count
-                        Raylib.DrawLineEx(v1, v2, 1, color);
-                    }
-                }
-
-                // draw particles
-                for (var i = 0; i < COUNT; i++)
-                {
-                    if (!s.ON[i]) continue;
-
-                    var pos = positions[i];
-                    var r = s.R[i] * RADIUS_MULTIPLIER;
-                    var m = s.M[i];
-                    var v = m < 1024
-                        ? 1.0
-                        : 1.0 - Math.Log(m / 1024.0) / Math.Log(32.0); // v(1k) = 1  v(32k) = 0
-                    var blue = (int)(255 * Math.Clamp(v, 0, 1));
-                    var color = new Color(255, 255, blue);
-                    Raylib.DrawCircle((int)pos.X, (int)pos.Y, r, color);
-                }
-
-                // TEXT OVERLAYS
-                {
-                    const string
-                        help1 = "Particles: [alive / total]",
-                        help2 = "Trail frames: [count / max count] [oldest / newest]",
-                        helpK = "Esc - Quit | F3 - Debug | Enter - Restart | \\ - Follow the Sun | Backspace - Pause | = - Step (when paused) | M1 - Drag space";
-                    var h = Raylib.GetScreenHeight();
-
-                    if (debug)
-                    {
-                        var row = 0;
-                        Raylib.DrawFPS(10, TextHeight());
-                        Raylib.DrawText($"{s.ActiveCount}/{COUNT}",                     10, TextHeight(), 20, Color.Blue);
-                        Raylib.DrawText($"{trails_frames_count}/{TRAIL_LEN_FRAMES}",    10, TextHeight(), 20, Color.Red);
-                        Raylib.DrawText($"{trails_frame_oldest}/{trails_frame_newest}", 10, TextHeight(), 20, Color.Red);
-                        Raylib.DrawText(help1,                                          10,       h - 40, 10, Color.Blue);
-                        Raylib.DrawText(help2,                                         150,       h - 40, 10, Color.Red);
-
-                        int TextHeight() => 10 + 30 * row++;
-                    }
-                    Raylib.DrawText(helpK,                                          10,       h - 20, 10, Color.LightGray);
-                }
-                Raylib.EndDrawing();
+                ResetTrails();
             }
         }
-        Raylib.CloseWindow();
+
+        var mouse_down = Raylib.IsMouseButtonDown(MouseButton.Left);
+        if (mouse_down)
+        {
+            follow_sun = false;
+            offset += Raylib.GetMouseDelta();
+        }
+    }
+
+    private void DoLogic()
+    {
+        if (follow_sun)
+        {
+            var c = Raylib.GetScreenCenter();
+            var sun = s.SunIndex;
+            offset.X = c.X - s.PX[sun];
+            offset.Y = c.Y - s.PY[sun];
+        }
+
+        trails_frame_newest = (trails_frame_newest + 1) % TRAIL_LEN_FRAMES;
+
+        if (trails_frames_count == TRAIL_LEN_FRAMES)
+            trails_frame_oldest = (trails_frame_oldest + 1) % TRAIL_LEN_FRAMES;
+        else
+            trails_frames_count++;
+
+        s.Tick();
+
+        for (var i = 0; i < COUNT; i++)
+        {
+            if (!s.ON[i])
+            {
+                ref var
+                    leftover_frames = ref trails_lo[i];
+                if (leftover_frames == -1)
+                    leftover_frames = trails_td[i] = trails_frames_count;
+                if (leftover_frames > 0 && trails_frames_count == TRAIL_LEN_FRAMES)
+                    leftover_frames--;
+
+                continue;
+            }
+
+            var x = offset.X + s.PX[i];
+            var y = offset.Y + s.PY[i];
+            positions[i] = new Vector2(x, y);
+            trails[COUNT * trails_frame_newest + i] = new Vector2(x, y);
+        }
+    }
+
+    private void DrawTrails()
+    {
+        for (var i = 0; i < trails_frames_count - 1; i++)
+        {
+            var f1 = (trails_frame_oldest + i) % TRAIL_LEN_FRAMES;
+            var f2 =                  (f1 + 1) % TRAIL_LEN_FRAMES;
+
+            var rb = (double)i / trails_frames_count; // relative brightness
+            var v = (byte)(128 * rb);
+            var color = new Color(v, v, v);
+
+            for (var j = 0; j < COUNT; j++) // for each particle
+            {
+                if (!s.ON[j])
+                {
+                    var lo = trails_lo[j]; // number of leftover frames
+                    if (lo == 0) continue;
+
+                    var tfc_at_death = trails_td[j];
+                    var skip_particle = trails_frames_count < TRAIL_LEN_FRAMES || tfc_at_death < trails_frames_count
+                        //              ^ buffer not full                      OR particle died when buffer wasn't full
+                        ? i >= tfc_at_death - 2 - trails_frame_oldest
+                        : i >= lo - 1;
+                    if (skip_particle) continue;
+                }
+
+                var v1 = trails[COUNT * f1 + j]; // [f0 coords] [f1 coords] [...] [fN coords]
+                var v2 = trails[COUNT * f2 + j]; //             \---------\ L = count
+                Raylib.DrawLineEx(v1, v2, 1, color);
+            }
+        }
+    }
+    private void DrawParticles()
+    {
+        for (var i = 0; i < COUNT; i++)
+        {
+            if (!s.ON[i]) continue;
+
+            var pos = positions[i];
+            var r = s.R[i] * RADIUS_MULTIPLIER;
+            var m = s.M[i];
+            var v = m < 1024
+                ? 1.0
+                : 1.0 - Math.Log(m / 1024.0) / Math.Log(32.0); // v(1k) = 1  v(32k) = 0
+            var blue = (int)(255 * Math.Clamp(v, 0, 1));
+            var color = new Color(255, 255, blue);
+            Raylib.DrawCircle((int)pos.X, (int)pos.Y, r, color);
+        }
+    }
+    private void DrawTextOverlays()
+    {
+        const string
+            help1 = "Particles: [alive / total]",
+            help2 = "Trail frames: [count / max count] [oldest / newest]",
+            helpK = "Esc - Quit | F3 - Debug | Enter - Restart | \\ - Follow the Sun | Backspace - Pause | = - Step (when paused) | M1 - Drag space";
+
+        var h = Raylib.GetScreenHeight();
+
+        if (debug)
+        {
+            var row = 0;
+
+            Raylib.DrawFPS(10, TextHeight());
+            Raylib.DrawText($"{s.ActiveCount}/{COUNT}",                     10, TextHeight(), 20, Color.Blue);
+            Raylib.DrawText($"{trails_frames_count}/{TRAIL_LEN_FRAMES}",    10, TextHeight(), 20, Color.Red);
+            Raylib.DrawText($"{trails_frame_oldest}/{trails_frame_newest}", 10, TextHeight(), 20, Color.Red);
+            Raylib.DrawText(help1,                                          10,       h - 40, 10, Color.Blue);
+            Raylib.DrawText(help2,                                         150,       h - 40, 10, Color.Red);
+
+            int TextHeight() => 10 + 30 * row++;
+        }
+        Raylib.DrawText(helpK, posX: 10, posY: h - 20, fontSize: 10, Color.LightGray);
     }
 }
 
--- GravitySimulator/Simulation.cs
+++ GravitySimulator/Simulation.cs
@@ -12,30 +12,35 @@ public class Simulation
         MAX_MASS_POW = 5,
         NO_MERGE = -1;
 
-    public   int   ActiveCount;
-    public   int   SunIndex;
+    // primary
     public  bool[] ON; // Status
     public float[] PX; // Position
     public float[] PY;
     public float[] VX; // Velocity
     public float[] VY;
+    public float[] M;  // Mass
+
+    // derivative
+    public   int   ActiveCount;
+    public   int   SunIndex;
+    public float[] R;  // Radius
+
+    // temporary
     public float[] AX; // Acceleration
     public float[] AY;
-    public float[] R;  // Radius
-    public float[] M;  // Mass
     public   int[] MI; // Merge into (index), merge list
 
     public void Init(int count, float width, float height, bool sun)
     {
-        var vmax = VELOCITY_MULTIPLIER * (sun ? 3 : 2);
+        var v = VELOCITY_MULTIPLIER * (sun ? 3 : 2);
         ActiveCount = count;
-        PX = InitList(count, 0, width);
-        PY = InitList(count, 0, height);
-        VX = InitList(count, -vmax, vmax);
-        VY = InitList(count, -vmax, vmax);
+        PX = InitList_AndFill(count, 0, width);
+        PY = InitList_AndFill(count, 0, height);
+        VX = InitList_AndFill(count, -v, v);
+        VY = InitList_AndFill(count, -v, v);
         AX = new float[count];
         AY = new float[count];
-        M  = InitList(count, 0, MAX_MASS_POW);
+        M  = InitList_AndFill(count, 0, MAX_MASS_POW);
         R  = new float[count];
         ON = new  bool[count];
         MI = new   int[count];
@@ -57,7 +62,7 @@ public class Simulation
         }
     }
 
-    private static float[] InitList(int count, float min, float max)
+    private static float[] InitList_AndFill(int count, float min, float max)
     {
         var list = new float[count];
         for (var i = 0; i < count; i++)
@@ -77,108 +82,45 @@ public class Simulation
         {
             if (!ON[i]) continue;
 
-            AX[i] = AY[i] = 0;
             for (var j = 0; j < count; j++) // account for gravity to all other particles
             {
                 if (j == i || !ON[j]) continue;
 
-                var dx = PX[i] - PX[j];
-                var dy = PY[i] - PY[j];
-                var d = Math.Sqrt(dx * dx + dy * dy);
-                var ri = R[i];
-                var rj = R[j];
-                if (d <= ri + rj) // add to merge list
-                {
-                    var huge  = ri > rj ? i : j;
-                    var tiny = ri > rj ? j : i;
-                    MI[tiny] = huge; // tiny store huge index, so 2+ tiny can be merged into 1 huge
-                    // todo fix tiny particle on big speed can escape merge by phasing thru huge one
-                }
-                var ax = G * M[j] * dx / (d * d * d);
-                var ay = G * M[j] * dy / (d * d * d);
-                AX[i] -= (float)ax;
-                AY[i] -= (float)ay;
+                AccelerateParticle(i, j);
             }
         }
 
-        for (int i = 0; i < count; i++)
+        // accelerate particles
+        for (var i = 0; i < count; i++)
         {
             if (!ON[i]) continue;
 
-            // affect velocity with acceleration
-            VX[i] += AX[i];
-            VY[i] += AY[i];
-            // affect position with velocity
-            PX[i] += VX[i];
-            PY[i] += VY[i];
+            ApplyAcceleration(i);
         }
 
-        // merge particles on merge list
-        // tiny one stores huge index!
+        // merge particles on the merge list
         var activeCount = ActiveCount;
         for (var i = 0; i < count; i++)
         {
             var tiny = i;
             var huge = MI[tiny];
-
             if (huge == NO_MERGE) continue;
 
-            if (!ON[huge]) // huge was merged into a REALLY HUGE
-            {
-                // merge tiny into the REALLY HUGE instead/
-                // or even better - into first in chain that is still alive
-                while (true)
-                {
-                    huge = MI[huge];
-                    if (ON[huge]) break; // NO_MERGE -> IOB
-                }
-            }
-            
-            // mass
-            var m1 = M[huge];
-            var m2 = M[tiny];
-            var mN = m1 + m2;
-            // Combine mass, update radius
-            M[huge] = mN;
-            R[huge] = CalculateRadius(huge);
-
-            // velocity
-            var vx1 = VX[huge];
-            var vy1 = VY[huge];
-            var vx2 = VX[tiny];
-            var vy2 = VY[tiny];
-            // Conserve momentum
-            VX[huge] = (m1 * vx1 + m2 * vx2) / mN;
-            VY[huge] = (m1 * vy1 + m2 * vy2) / mN;
-
-            // position
-            var x1 = PX[huge];
-            var y1 = PY[huge];
-            var x2 = PX[tiny];
-            var y2 = PY[tiny];
-            // Put particle into center of mass
-            PX[huge] = (m1 * x1 + m2 * x2) / mN;
-            PY[huge] = (m1 * y1 + m2 * y2) / mN;
-
-            ON[tiny] = false;
-            ActiveCount--;
+            MergeParticles(huge, tiny);
         }
 
-        // clear merge list
+        // reset temporary lists
         for (var i = 0; i < count; i++)
         {
-            MI[i] = -1;
+            MI[i] = NO_MERGE;
+            AX[i] = AY[i] = 0;
         }
 
+        // find biggest particle (if it may have changed)
         if (activeCount != ActiveCount)
         {
             SunIndex = FindSunIndex();
         }
-    }
-
-    private float CalculateRadius(int i)
-    {
-        return (float)Math.Pow(0.75 * M[i] / Math.PI, 1 / 3.0);
     }
 
     private int FindSunIndex()
@@ -194,7 +136,82 @@ public class Simulation
                 sun_i = i;
             }
         }
-
         return sun_i;
+    }
+
+    private void AccelerateParticle(int i, /* with */ int j)
+    {
+        var dx = PX[i] - PX[j];
+        var dy = PY[i] - PY[j];
+        var ri = R[i];
+        var rj = R[j];
+        var d = Math.Sqrt(dx * dx + dy * dy);
+        if (d <= ri + rj) // merge particles
+        {
+            var huge = ri > rj ? i : j;
+            var tiny = ri > rj ? j : i;
+            MI[tiny] = huge; // tiny store huge index, so 2+ tiny can be merged into 1 huge
+        }
+        var m = M[j];
+        var ax = G * m * dx / (d * d * d);
+        var ay = G * m * dy / (d * d * d);
+        AX[i] -= (float)ax;
+        AY[i] -= (float)ay;
+    }
+
+    private void ApplyAcceleration(int i)
+    {
+        // affect velocity with acceleration
+        VX[i] += AX[i];
+        VY[i] += AY[i];
+        // affect position with velocity
+        PX[i] += VX[i];
+        PY[i] += VY[i];
+    }
+
+    private void MergeParticles(int huge, int tiny)
+    {
+        if (!ON[huge]) // huge was already merged into an even bigger one
+        {
+            while (true) // find first alive in the merge chain instead
+            {
+                huge = MI[huge];
+                if (ON[huge]) break; // NO_MERGE -> IOB
+            }
+        }
+
+        // mass
+        var m1 = M[huge];
+        var m2 = M[tiny];
+        var mN = m1 + m2;
+        // Combine mass, update radius
+        M[huge] = mN;
+        R[huge] = CalculateRadius(huge);
+
+        // velocity
+        var vx1 = VX[huge];
+        var vy1 = VY[huge];
+        var vx2 = VX[tiny];
+        var vy2 = VY[tiny];
+        // Conserve momentum
+        VX[huge] = (m1 * vx1 + m2 * vx2) / mN;
+        VY[huge] = (m1 * vy1 + m2 * vy2) / mN;
+
+        // position
+        var x1 = PX[huge];
+        var y1 = PY[huge];
+        var x2 = PX[tiny];
+        var y2 = PY[tiny];
+        // Put particle into center of mass
+        PX[huge] = (m1 * x1 + m2 * x2) / mN;
+        PY[huge] = (m1 * y1 + m2 * y2) / mN;
+
+        ON[tiny] = false;
+        ActiveCount--;
+    }
+
+    private float CalculateRadius(int i)
+    {
+        return (float)Math.Pow(0.75 * M[i] / Math.PI, 1 / 3.0);
     }
 }
